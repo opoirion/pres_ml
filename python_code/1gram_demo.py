79abe212696735fb2ae81bc73b9ecc18d2af6938
olivier poirion <o.poirion@gmail.com>
2015-12-04 15:16:34 +0100

    iter 3

--- python_code/1gram_demo.py
+++ python_code/1gram_demo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-
+from sklearn.feature_extraction.text import CountVectorizer
+from scipy.spatial.distance import jaccard
 
 training_set = [
     "super ce blog! J'adore ce truc...",
@@ -12,6 +12,8 @@ training_set = [
     "pas super ce blog, peut mieux faire je n'y reviendrai pas"
 ]
 
+
+
 status = [
     "good comment",
     "good comment",
@@ -21,14 +23,24 @@ status = [
     "bad comment"
 ]
 
+new_comments = [
+    "c'est un super commentaire positif",
+    "pas super"
+]
+
 def main():
-    vectorizer = TfidfVectorizer(
-        ngram_range=(1,1),
-        use_idf=False,
-        smooth_idf = False,
-        norm=None)
+    vectorizer = CountVectorizer(ngram_range=(1,2),max_df=1.0, min_df=0.0)
+
     matrix = vectorizer.fit_transform(training_set)
-    print matrix
+    for new_comment in new_comments:
+        print '\n####\ncommentaire:{0}\n'.format(new_comment)
+        vector = vectorizer.transform([new_comment])
+
+        i = 0
+        for vect in matrix:
+            score = jaccard(vect.todense(), vector.todense())
+            print 'sentence: {0}"\tscore:{1}'.format(training_set[i], score)
+            i += 1
 
 if __name__ == "__main__":
     main()
